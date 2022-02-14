9522e53dcf09ed1396d7274cdb508165730d7e84
back-jond1992 <b.jond7@outlook.com>
2022-02-14 20:15:16 +0000

    starts C practice

---
+++ sampleProgram.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+printf("\nA sample C program\n\n");
+printf("Hello World!\n");
+printf("   /|\n");
+printf("  / |\n");
+printf(" /  |\n");
+printf("/___|\n");
+
+/*variables and data types*/
+
+char name[] = "Jack";
+int age = 29;
+printf("Hello my name is %s\n", name);
+printf("I am %d years old\n", age);
+
+age = 28;
+printf("Hello my name is Serin \n");
+printf("I am %d years old\n", age);
+
+
+int year = 2022; /* integer - whole number */
+double score = 2.36; /* decimal number */
+char letter = 'w'; /* single character - string */
+char letters[] = "word"; /* string of strings - multiple characters */
+
+printf("\"Jack\"\n\"Serin\"\n\"James\"\n");
+
+return 0;
+}
+
+
